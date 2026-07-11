7bf219e98e05402731f7a5d6cd9b0246a8b2f9f3
shubhamsirse507-jpg <shubhamsirse507@gmail.com>
2026-07-11 20:41:10 +0530

    Change DB connection to remote MySQL server
    Updated database connection parameters to use a remote MySQL server.

--- db.py
+++ db.py
@@ -1,10 +1,16 @@
 import mysql.connector
+from mysql.connector import Error
 
 def get_connection():
-    return mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="Shubham@1978",
-        database="hms_db"
-    )
-print("Db connected")
+    try:
+        connection = mysql.connector.connect(
+            host="sql12.freesqldatabase.com",
+            user="sql12832782",          # freesqldatabase.com uses same name as DB for username
+            password="ypxHYNxUHt",    # the password you were given when the DB was created
+            database="sql12832782",
+            port=3306
+        )
+        return connection
+    except Error as e:
+        print(f"Database connection failed: {e}")
+        return None
